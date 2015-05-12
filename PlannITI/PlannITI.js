e7d2676b145304dd1a4bfc3f2225ebbef8a8c0b3
Erwandebenaze <duhildebenaze@intechinfo.fr>
2015-05-12 10:22:58 +0200

    Hey Jude

--- PlannITI/PlannITI.js
+++ PlannITI/PlannITI.js
@@ -1,10 +1,15 @@
-﻿var app = angular.module('plannITI', ['ngRoute'])
-.config(function ($routeProvider) {
+﻿var plann = angular.module( 'plann', ['ngRoute', 'teachersController', 'roomsController'] );
+plann.config( ['$routeProvider', function ( $routeProvider ) {
 
-    $routeProvider.when('/teachers', {
-        templateUrl: '/View/VTeacher.html',
-        controller: 'FormVMTeachers'
-    });
-    $routeProvider.otherwise({ redirectTo: '/teachers'});
+    $routeProvider.
+        when( '/teachers', {
+            templateUrl: '/View/VTeacher.html',
+            controller: 'FormVMTeachers'
+        } ).
+                when( '/rooms', {
+                    templateUrl: '/View/VRoom.html',
+                    controller: 'FormVMRooms'
+                } ).
+      otherwise( { redirectTo: '/teachers' } );
 
-});
+}] );

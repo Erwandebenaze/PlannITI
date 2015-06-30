b275a561649225d35f823f14ead0746c727a6c87
Erwan de Bénazé <duhildebenaze@intechinfo.fr>
2015-06-30 15:13:33 +0200

    YOUHOU

--- Plann.Interface/UcMgtPeriod.Designer.cs
+++ Plann.Interface/UcMgtPeriod.Designer.cs
@@ -34,18 +34,15 @@
             System.Windows.Forms.Label label4;
             this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
             this.nameTextBox = new System.Windows.Forms.TextBox();
-            this.begginingButton = new System.Windows.Forms.Button();
-            this.begginingDateText = new System.Windows.Forms.Label();
             this.endingDateText = new System.Windows.Forms.Label();
-            this.endingButton = new System.Windows.Forms.Button();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.monthCalendar2 = new System.Windows.Forms.MonthCalendar();
             this.holidaysButton = new System.Windows.Forms.Button();
-            this.objectListView1 = new BrightIdeasSoftware.ObjectListView();
-            this.olvColumn1 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
             this.validatePeriod = new System.Windows.Forms.Button();
             this.begginingDateTextBox = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.endingDateTextBox = new System.Windows.Forms.TextBox();
+            this.objectListView1 = new BrightIdeasSoftware.ObjectListView();
+            this.olvColumn1 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
             label1 = new System.Windows.Forms.Label();
             label2 = new System.Windows.Forms.Label();
             label3 = new System.Windows.Forms.Label();
@@ -104,24 +101,6 @@
             this.nameTextBox.Size = new System.Drawing.Size(100, 22);
             this.nameTextBox.TabIndex = 4;
             // 
-            // begginingButton
-            // 
-            this.begginingButton.Location = new System.Drawing.Point(134, 95);
-            this.begginingButton.Name = "begginingButton";
-            this.begginingButton.Size = new System.Drawing.Size(75, 30);
-            this.begginingButton.TabIndex = 5;
-            this.begginingButton.Text = "Choisir";
-            this.begginingButton.UseVisualStyleBackColor = true;
-            this.begginingButton.Click += new System.EventHandler(this.begginingButton_Click);
-            // 
-            // begginingDateText
-            // 
-            this.begginingDateText.AutoSize = true;
-            this.begginingDateText.Location = new System.Drawing.Point(134, 73);
-            this.begginingDateText.Name = "begginingDateText";
-            this.begginingDateText.Size = new System.Drawing.Size(0, 17);
-            this.begginingDateText.TabIndex = 6;
-            // 
             // endingDateText
             // 
             this.endingDateText.AutoSize = true;
@@ -129,16 +108,6 @@
             this.endingDateText.Name = "endingDateText";
             this.endingDateText.Size = new System.Drawing.Size(0, 17);
             this.endingDateText.TabIndex = 7;
-            // 
-            // endingButton
-            // 
-            this.endingButton.Location = new System.Drawing.Point(227, 95);
-            this.endingButton.Name = "endingButton";
-            this.endingButton.Size = new System.Drawing.Size(75, 30);
-            this.endingButton.TabIndex = 8;
-            this.endingButton.Text = "Choisir";
-            this.endingButton.UseVisualStyleBackColor = true;
-            this.endingButton.Click += new System.EventHandler(this.endingButton_Click);
             // 
             // linkLabel1
             // 
@@ -169,6 +138,34 @@
             this.holidaysButton.UseVisualStyleBackColor = true;
             this.holidaysButton.Click += new System.EventHandler(this.holidaysButton_Click);
             // 
+            // validatePeriod
+            // 
+            this.validatePeriod.Location = new System.Drawing.Point(255, 395);
+            this.validatePeriod.Name = "validatePeriod";
+            this.validatePeriod.Size = new System.Drawing.Size(75, 243);
+            this.validatePeriod.TabIndex = 14;
+            this.validatePeriod.Text = "Valider la période";
+            this.validatePeriod.UseVisualStyleBackColor = true;
+            this.validatePeriod.Click += new System.EventHandler(this.validatePeriod_Click);
+            // 
+            // begginingDateTextBox
+            // 
+            this.begginingDateTextBox.Location = new System.Drawing.Point(122, 67);
+            this.begginingDateTextBox.Name = "begginingDateTextBox";
+            this.begginingDateTextBox.Size = new System.Drawing.Size(128, 22);
+            this.begginingDateTextBox.TabIndex = 15;
+            this.begginingDateTextBox.Text = "Cliquez puis";
+            this.begginingDateTextBox.Click += new System.EventHandler(this.begginingDateTextBox_Click);
+            // 
+            // endingDateTextBox
+            // 
+            this.endingDateTextBox.Location = new System.Drawing.Point(228, 67);
+            this.endingDateTextBox.Name = "endingDateTextBox";
+            this.endingDateTextBox.Size = new System.Drawing.Size(137, 22);
+            this.endingDateTextBox.TabIndex = 16;
+            this.endingDateTextBox.Text = "sélectionnez une date";
+            this.endingDateTextBox.Click += new System.EventHandler(this.endingDateTextBox_Click);
+            // 
             // objectListView1
             // 
             this.objectListView1.AllColumns.Add(this.olvColumn1);
@@ -189,35 +186,11 @@
             this.olvColumn1.UseFiltering = false;
             this.olvColumn1.Width = 150;
             // 
-            // validatePeriod
-            // 
-            this.validatePeriod.Location = new System.Drawing.Point(255, 395);
-            this.validatePeriod.Name = "validatePeriod";
-            this.validatePeriod.Size = new System.Drawing.Size(75, 243);
-            this.validatePeriod.TabIndex = 14;
-            this.validatePeriod.Text = "Valider la période";
-            this.validatePeriod.UseVisualStyleBackColor = true;
-            this.validatePeriod.Click += new System.EventHandler(this.validatePeriod_Click);
-            // 
-            // begginingDateTextBox
-            // 
-            this.begginingDateTextBox.Location = new System.Drawing.Point(109, 117);
-            this.begginingDateTextBox.Name = "begginingDateTextBox";
-            this.begginingDateTextBox.Size = new System.Drawing.Size(100, 22);
-            this.begginingDateTextBox.TabIndex = 15;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(248, 107);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 16;
-            // 
             // UcMgtPeriod
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.endingDateTextBox);
             this.Controls.Add(this.begginingDateTextBox);
             this.Controls.Add(this.validatePeriod);
             this.Controls.Add(this.objectListView1);
@@ -225,10 +198,7 @@
             this.Controls.Add(label4);
             this.Controls.Add(this.monthCalendar2);
             this.Controls.Add(this.linkLabel1);
-            this.Controls.Add(this.endingButton);
             this.Controls.Add(this.endingDateText);
-            this.Controls.Add(this.begginingDateText);
-            this.Controls.Add(this.begginingButton);
             this.Controls.Add(this.nameTextBox);
             this.Controls.Add(this.monthCalendar1);
             this.Controls.Add(label3);
@@ -246,10 +216,7 @@
 
         private System.Windows.Forms.MonthCalendar monthCalendar1;
         private System.Windows.Forms.TextBox nameTextBox;
-        private System.Windows.Forms.Button begginingButton;
-        private System.Windows.Forms.Label begginingDateText;
         private System.Windows.Forms.Label endingDateText;
-        private System.Windows.Forms.Button endingButton;
         private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.MonthCalendar monthCalendar2;
         private System.Windows.Forms.Button holidaysButton;
@@ -257,6 +224,6 @@
         private BrightIdeasSoftware.OLVColumn olvColumn1;
         private System.Windows.Forms.Button validatePeriod;
         private System.Windows.Forms.TextBox begginingDateTextBox;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox endingDateTextBox;
     }
 }
